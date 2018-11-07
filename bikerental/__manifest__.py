80215af465bd5532df64f738be33b1b3afb5c032
nurlanf <nurlan.farajov@gmail.com>
2018-11-08 00:47:25 +0100

    adding bikerental module

--- bikerental/__manifest__.py
+++ bikerental/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/ir_model_access.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
